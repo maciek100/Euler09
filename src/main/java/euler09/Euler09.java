8c7cbefd604cfcf3fa3aed105882d9b0f9c125c0
Maciej Stoszko <maciek100@gmail.com>
2020-04-17 16:13:21 -0500

    More tests ...

--- src/main/java/euler09/Euler09.java
+++ src/main/java/euler09/Euler09.java
@@ -2,37 +2,47 @@ package euler09;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /* Euler 9.
  * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
  * a^2 + b^2 = c^2
- * For example, 32 + 42 = 9 + 16 = 25 = 52.
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 => (9 + 16 + 25 = 50).
  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  * Find the product abc.
  */
 // correct solution = 31875000
 public class Euler09 {
-
-	public static long triplet (int looked) {
-		long result = 0L;
-		for (int a = 1; a < looked; a++) {
-			for (int b = a; b < looked; b++) {
-				for (int c = b; c < looked; c++) {
-					if (a * a + b * b == c * c && a + b + c == looked) {
-						result = a * b * c;
+	// Very brute force approach
+	public static long triplet (int linearSum) {
+		long product = 0L;
+		for (int a = 1; a < linearSum/2; a++) {
+			for (int b = a; b < linearSum/2; b++) {
+				for (int c = b; c < linearSum/2; c++) {
+					if (condition(a, b, c, linearSum)) {
+						product = a * b * c;
 						System.out.printf("Pythagorean [%d,%d,%d] triplet = %d\n",
-								a,b,c,result);
+								a, b, c, product);
 					}
 				}
 			}
 		}
-		return result;
+		return product;
+	}
+
+	private static boolean condition(int a, int b, int c, int sum) {
+		return a * a + b * b  == c * c  && a + b + c == sum;
 	}
 
 	@Test
+	public void conditionTest() {
+		int sum = 3 + 4 + 5;
+		assertTrue("", condition(3,4,5, sum));
+	}
+	@Test
 	public void testEuler09 () {
-		long correctOne = 31875000L;
-		assertEquals("", correctOne, triplet(1000));
+		long sumOfSquares = 31875000L;
+		assertEquals("", sumOfSquares, triplet(1000));
 	}
 
 }
